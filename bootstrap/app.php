986542457347052ada817f4d7d4d0e46c6fea143
اسم و فامیل شما <reza.gamer.alt1@gmail.com>
2026-01-02 01:55:00 +0330

    M05: register Spatie permission middleware alias for web routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,9 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\ForceJsonResponse::class,
         ]);
         
-        // Register feature middleware alias
+        // Register middleware aliases
         $middleware->alias([
             'feature' => \App\Http\Middleware\EnsureFeatureEnabled::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
